c24f84fa090b23ff99ff93900c6ac9cf70d57d01
habinski <matthewschmtz@gmail.com>
2021-04-15 01:09:42 +0200

    work on side card

--- src/components/header/sideCart.js
+++ src/components/header/sideCart.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react"
-
+import { Link } from "gatsby"
 import CartIcon from '../../images/icons/cart.svg'
 import CloseIcon from '../../images/icons/close.svg'
 import { useCart } from 'react-use-cart'
 
-import { totalUnique, openBtn, sideCart, side, cart, openCart, closeBtn } from './sideCart.module.scss'
+import { totalUnique, openBtn, sideCart, side, cart, openCart, closeBtn, cartItems, total, greySpan, toOrder } from './sideCart.module.scss'
 import CartItem from "./sideCartItem"
 
 const SideCart = () => {
-	const { totalUniqueItems, items } = useCart()
+	const { totalUniqueItems, items, cartTotal } = useCart()
 	const [cartToggle, setCartToggle] = useState()
 
 	// console.log(items)
@@ -21,14 +21,19 @@ const SideCart = () => {
 				<div className={cart}>
 					<h5>У кошику: <span>{totalUniqueItems}</span></h5>
 					<span className={closeBtn} onClick={() => { setCartToggle(!cartToggle) }}><CloseIcon /></span>
-					{
-						items.map(item => {
-							return (
-								// <p>item</p>
-								<CartItem key={item.id} product={item} />
-							)
-						})
-					}
+					<div className={cartItems}>
+						{
+							items.map(item => {
+								return (
+									<CartItem key={item.id} product={item} />
+								)
+							})
+						}
+					</div>
+					<div className={total}>
+						<p>Ціна: <span>{cartTotal}₴</span> <span className={greySpan} >+ доставка</span></p>
+						<Link className={toOrder} to="/order">Перейти до замовлення</Link>
+					</div>
 				</div>
 			</div>
 		</>
